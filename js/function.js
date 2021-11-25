fdd299d6937a46cb2a4c702a0c9f2662b09e4881
shruthi093 <jayasruthi.l@gmail.com>
2021-11-25 20:40:46 +0530

    new js file

---
+++ js/function.js
@@ -0,0 +1,33 @@
+/*let arr=["john",20909,9.0,"react"]
+//foreach
+/*arr.forEach(
+    (element,index)=>{
+        console.log(element)
+        console.log(index)
+    }
+)*/
+// push pop shift unshift
+/*arr.push("node js",5,"mongodb")
+arr.unshift("CSE","clgname")
+for(let i=0;i<=5;i++){
+    arr.shift()
+}
+console.log(arr)*/
+//removing elements 
+/*arr.splice(2,3,"one","two","three","four")*/
+/*console.log(arr.unshift("CSE","clgname"))
+console.log(arr.slice(0,4))
+console.log(arr.slice(-6,-1))
+//arr.unshift("CSE","clgname")
+//console.log(arr)
+//arr.slice(2,5)
+//console.log(arr)
+/*
+let greet=(fname)=>{
+console.log(fname)
+}
+console.log(greet("john"))*/
+/*let newArr=arr.concat(["new","item"])
+console.log(newArr)
+console.log(arr)*/
+
