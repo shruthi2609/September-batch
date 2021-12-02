a2d048afd46b7b15f943e7f60e7af53cf1ca1a94
shruthi093 <jayasruthi.l@gmail.com>
2021-12-02 20:15:17 +0530

    array methods

--- js/function.js
+++ js/function.js
@@ -30,4 +30,65 @@ console.log(greet("john"))*/
 /*let newArr=arr.concat(["new","item"])
 console.log(newArr)
 console.log(arr)*/
+//indexOf lastIndexOf includes
+let arr=["john",20909,9.0,20909,"react",20909]
+console.log(arr.includes("john"))
+/*function findIndex(arr,val){
+arr.forEach(
+    (element,index)=>{
+        if(element===val){
+            console.log(index)
+        }
+    })
+}
+findIndex(arr,20909)
+function findFirstIndex(arr,val){
+   for(let i=0;i<arr.length;i++)
+   {
+    if(arr[i]===val){
+        continue
+    }
+    else{
+        console.log(i)//0 2 4
+    }
+    
+   }
+    }
+findFirstIndex(arr,20909)
+function findLastIndex(arr,val){
+    result=null
+    arr.forEach(
+        (element,index)=>{
+            if(element===val){
+                result=index
+            }
+        })
+    return result
+    }
+console.log(findLastIndex(arr,20909))
+
+
+//find 
+const result=student.find((item)=>item.company==="tcs"
+)
+console.log(result.fname,result.reg)*/
+//filter
+/*let student=[
+        {reg:123,fname:"peter",company:"tcs"},
+        {reg:122,fname:"john",company:"cts"},
+        {reg:121,fname:"george",company:"tcs"},
+]
+const result=student.filter((item)=>item.company==="tcs"
+)
+result.forEach(
+    (element)=>{
+       console.log(element.fname,element.reg) 
+    })
+result.map((item)=>console.log(item.fname,item.reg))*/
+
+
+
+
+
+
 
