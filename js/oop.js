3ea96a2337b5e1ac888fadd5b551681d168706e2
shruthi093 <jayasruthi.l@gmail.com>
2021-12-02 20:42:15 +0530

    classes

---
+++ js/oop.js
@@ -0,0 +1,16 @@
+class Student{
+    //data member - age
+    //member method - modifyAge
+    constructor(n,r){
+     this.fname=n;
+     this.reg=r
+    }
+}
+let student1=new Student("john",123)
+let student2=new Student("peter",121)
+console.log(student1.fname)
+// class car 
+// class employee
+
+
+
